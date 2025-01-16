ac98a97f90138aafbbfda5c96e9d2b9016c24e8b
deseansoh <deseansyc@gmail.com>
2025-01-16 21:27:15 +0800

    Add Entry class

---
+++ src/main/java/Entry.java
@@ -0,0 +1,11 @@
+public class Entry {
+    private String name;
+
+    public Entry(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
