65775e41611d3dfbc29434729ce4bfb7f854cddc
deseansoh <deseansyc@gmail.com>
2025-01-16 21:13:28 +0800

    Abstract getUserInput behaviour

--- src/main/java/ThoughtBot.java
+++ src/main/java/ThoughtBot.java
@@ -1,32 +1,37 @@
 import java.util.Scanner;
 
 public class ThoughtBot {
+    static final String SEPARATOR = "    ________________________________________";
+    static final String SEPARATORNEWLINE = "    ________________________________________\n";
+
+    static final String GREETING = SEPARATORNEWLINE
+            + "    Hello there! I'm ThoughtBot\n"
+            + "    How may I help you today?\n"
+            + SEPARATORNEWLINE;
+
+    static final String GOODBYE = "    Goodbye! Have a great week ahead :)\n"
+            + SEPARATOR;
+
     public static void main(String[] args) {
-        final String SEPARATOR = "________________________________________";
-
-        final String GREETING = "________________________________________\n"
-                + "Hello there! I'm ThoughtBot\n"
-                + "How may I help you today?\n"
-                + SEPARATOR;
-
-        final String GOODBYE = "Goodbye! Have a great week ahead :)\n"
-                + SEPARATOR;
-
         String userInput;
         Scanner userInputScanner = new Scanner(System.in);
 
         System.out.println(GREETING);
 
-        userInput = userInputScanner.nextLine();
-        System.out.println(SEPARATOR);
+        userInput = getUserInput(userInputScanner);
 
-        while (!userInput.toLowerCase().equals("bye")) {
-            System.out.println(userInput + "\n" + SEPARATOR);
+        while (!userInput.equalsIgnoreCase("bye")) {
+            System.out.println(userInput + "\n" + SEPARATORNEWLINE);
 
-            userInput = userInputScanner.nextLine();
-            System.out.println(SEPARATOR);
+            userInput = getUserInput(userInputScanner);
         }
 
         System.out.println(GOODBYE);
+    }
+
+    public static String getUserInput(Scanner userInputScanner) {
+        String userInput = userInputScanner.nextLine();
+        System.out.println(SEPARATOR);
+        return userInput;
     }
 }
