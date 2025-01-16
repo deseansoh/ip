c0d982f1d857c09bc0df716a257605cf8d9d47ab
deseansoh <deseansyc@gmail.com>
2025-01-16 22:40:54 +0800

    Add done functionality to Entry

--- src/main/java/Entry.java
+++ src/main/java/Entry.java
@@ -1,11 +1,35 @@
 public class Entry {
     private String name;
+    private boolean done;
 
     public Entry(String name) {
         this.name = name;
+        this.done = false;
     }
 
     public String getName() {
-        return this.name;
+        if (done) {
+            return "[X] " + this.name;
+        }
+
+        return "[ ] " + this.name;
+    }
+
+    public boolean markDone() {
+        if (!done) {
+            this.done = true;
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean markUndone() {
+        if (done) {
+            this.done = false;
+            return true;
+        }
+
+        return false;
     }
 }
