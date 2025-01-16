83b0291ee612cec7083247e4257c21329806fc38
deseansoh <deseansyc@gmail.com>
2025-01-16 22:13:03 +0800

    Add methods addEntry and displayTaskList to TaskList

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -6,4 +6,22 @@ public class TaskList {
     public TaskList() {
         this.entries = new ArrayList<>(100);
     }
+
+    public void addEntry(String entryName) {
+        this.entries.add(new Entry(entryName));
+    }
+
+    public String displayTaskList() {
+        String stringForm = "";
+        int currentNumber = 1;
+
+        for (Entry e: entries) {
+            String numberString = String.valueOf(currentNumber);
+            currentNumber++;
+
+            stringForm = stringForm + (numberString + ". " + e.getName() + "\n");
+        }
+
+        return stringForm;
+    }
 }
