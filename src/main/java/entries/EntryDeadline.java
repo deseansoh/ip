f5fa8c341f6d21082fdb445b02aff7320d9a8449
deseansoh <deseansyc@gmail.com>
2025-01-17 18:15:09 +0800

    Add EntryDeadline

---
+++ src/main/java/entries/EntryDeadline.java
@@ -0,0 +1,15 @@
+package entries;
+
+public class EntryDeadline extends Entry {
+    private String deadline;
+
+    public EntryDeadline(String name, String deadline) {
+        super(name);
+        this.deadline = deadline;
+    }
+
+    @Override
+    public String getName() {
+        return "[D]" + super.getName() + " (by:" + deadline + ")";
+    }
+}
