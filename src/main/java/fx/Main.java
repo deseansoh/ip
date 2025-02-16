4e72a81b830199659261ea08a5a343806041275e
deseansoh <deseansyc@gmail.com>
2025-02-16 16:40:18 +0800

    Edit FX to show title

--- src/main/java/fx/Main.java
+++ src/main/java/fx/Main.java
@@ -23,6 +23,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("ThoughtBot");
             fxmlLoader.<MainWindow>getController().setThoughtBot(thoughtBot); // inject the ThoughtBot instance
             stage.show();
         } catch (IOException e) {
